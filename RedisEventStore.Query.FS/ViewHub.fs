44baa760b87927f636ec978bba8acf27c20d1b6a
geandbe <gene.belitski@gmail.com>
2014-06-16 05:36:23 -0400

    Threw in F# ViewHub

---
+++ RedisEventStore.Query.FS/ViewHub.fs
@@ -0,0 +1,47 @@
+﻿namespace ViewHubHost
+
+open System
+
+open Owin
+open Microsoft.AspNet.SignalR
+open Microsoft.AspNet.SignalR.Hubs
+open Microsoft.Owin.Hosting
+open Microsoft.Owin.Cors
+
+open ServiceStack.Redis
+
+open EkonBenefits.FSharp.Dynamic
+
+open BlurocketTest.RedisAggregator
+
+type ViewHub() =
+    inherit Hub()
+
+
+module Kick_In =
+
+    let startup (app: IAppBuilder) =
+            app.UseCors CorsOptions.AllowAll |> ignore
+            app.MapSignalR() |> ignore
+
+    let updateA newAnalytics =
+        GlobalHost.ConnectionManager.GetHubContext<ViewHub>().Clients.All?UpdateAnalytics newAnalytics
+
+    let updateM newMax =
+        GlobalHost.ConnectionManager.GetHubContext<ViewHub>().Clients.All?UpdateMax newMax
+
+    [<EntryPoint>]
+    let main argv =
+        let hostUrl = @"http://localhost:8080"
+        use app = WebApp.Start(hostUrl,startup)
+        printfn "SignalR F# ViewHub is running on %s" hostUrl
+        use redisClient = new RedisClient()
+        let typedClient = redisClient.As<String>()
+        let eventQ = typedClient.Lists.["urn:blurocket:events"]
+        while true do
+           match (typedClient.BlockingDequeueItemFromList(eventQ, Nullable())) with
+           | "analytics" -> updateA (redisClient.Get<Analytics> "urn:blurocket:analytics")
+           | "max" -> updateM (redisClient.Get<int> "urn:blurocket:max")
+           | _ -> failwith "Unknown EventStore descriptor"
+
+        0 // return an integer exit code
